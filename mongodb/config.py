7c2eecdce89da1da067e43cf436dce08d957860a
davidpissarra <david.pissarra@tecnico.ulisboa.pt>
2021-12-15 03:18:02 +0000

    Bulk load and read collection implemented.

--- mongodb/config.py
+++ mongodb/config.py
@@ -1,8 +1,8 @@
-import pymongo.cursor
+from pymongo import InsertOne, DeleteMany, ReplaceOne, UpdateOne
 from pymongo import MongoClient
 from query_manager import *
+import numpy as np
 import json
-
 
 class MongoConn:
     @classmethod
@@ -27,6 +27,11 @@ class MongoCollections:
         return {'science': MongoDatabase.database().article_science,
                 'technology': MongoDatabase.database().article_tech}
 
+    @classmethod
+    def read(self):
+        return {'Beijing': MongoDatabase.database().read_beijing,
+                'Hong Kong': MongoDatabase.database().read_hong_kong}
+
 
 class MongoInit:
     def __init__(self):
@@ -34,35 +39,86 @@ class MongoInit:
         self.user_path = 'user.dat'
         self.article_path = 'article.dat'
         self.read_path = 'read.dat'
+        self.bulk_size = 10000
+        self.bulk_size_read = 50000
 
     def init_user(self):
         user_file = open(self.root_dir + self.user_path, 'r')
+        bulk_counter, bulk_dict = 0, {'Beijing': [], 'Hong Kong': []}
         for line in user_file.readlines():
-            entry = json.loads(line)
-            MongoCollections.user()[entry['region']].insert_one(entry)
+            bulk_counter += 1
+            doc = json.loads(line)
+            bulk_dict[doc['region']] += [InsertOne(doc)]
+            if bulk_counter == self.bulk_size:
+                MongoCollections.user()['Beijing'].bulk_write(bulk_dict['Beijing'], ordered=True)
+                MongoCollections.user()['Hong Kong'].bulk_write(bulk_dict['Hong Kong'], ordered=True)
+                bulk_counter = 0
+                bulk_dict['Beijing'], bulk_dict['Hong Kong'] = [], []
+        if bulk_dict['Beijing']:
+            MongoCollections.user()['Beijing'].bulk_write(bulk_dict['Beijing'], ordered=True)
+        if bulk_dict['Hong Kong']:
+            MongoCollections.user()['Hong Kong'].bulk_write(bulk_dict['Hong Kong'], ordered=True)
         user_file.close()
 
     def init_article(self):
         article_file = open(self.root_dir + self.article_path, 'r')
+        bulk_counter, bulk_dict = 0, {'science': [], 'technology': []}
         for line in article_file.readlines():
-            entry = json.loads(line)
-            MongoCollections.article()[entry['category']].insert_one(entry)
+            bulk_counter += 1
+            doc = json.loads(line)
+            bulk_dict[doc['category']] += [InsertOne(doc)]
+            if bulk_counter == self.bulk_size:
+                MongoCollections.article()['science'].bulk_write(bulk_dict['science'], ordered=True)
+                MongoCollections.article()['technology'].bulk_write(bulk_dict['technology'], ordered=True)
+                bulk_counter = 0
+                bulk_dict['science'], bulk_dict['technology'] = [], []
+        if bulk_dict['science']:
+            MongoCollections.article()['science'].bulk_write(bulk_dict['science'], ordered=True)
+        if bulk_dict['technology']:
+            MongoCollections.article()['technology'].bulk_write(bulk_dict['technology'], ordered=True)
         article_file.close()
 
     def init_read(self):
         read_file = open(self.root_dir + self.read_path, 'r')
+        bulk_counter, bulk_list, uids = 0, [], []
         for line in read_file.readlines():
-            entry = json.loads(line)
-            MongoCollections.user()[entry.region].insert_one(entry)
+            bulk_counter += 1
+            doc = json.loads(line)
+            uids += [doc['uid']]
+            bulk_list += [InsertOne(doc)]
+            if bulk_counter == self.bulk_size_read:
+                regions = QueryManager.query_user({'uid': {'$in': uids}}, {'uid': 1, 'region': 1})
+                regions = dict(map(lambda x: (x['uid'], x['region']), regions))
+                regions = list(map(lambda x: regions[x], uids))
+                beijing_index = np.argwhere(np.array(regions) == 'Beijing').reshape(-1)
+                hk_index = np.argwhere(np.array(regions) == 'Hong Kong').reshape(-1)
+                bulk_list = np.array(bulk_list)
+                MongoCollections.read()['Beijing'].bulk_write(list(bulk_list[beijing_index]), ordered=True)
+                MongoCollections.read()['Hong Kong'].bulk_write(list(bulk_list[hk_index]), ordered=True)
+                bulk_counter = 0
+                bulk_list, uids = [], []
+        if bulk_counter > 0:
+            regions = QueryManager.query_user({'uid': {'$in': uids}}, {'uid': 1, 'region': 1})
+            regions = dict(map(lambda x: (x['uid'], x['region']), regions))
+            regions = list(map(lambda x: regions[x], uids))
+            beijing_index = np.argwhere(np.array(regions) == 'Beijing').reshape(-1)
+            hk_index = np.argwhere(np.array(regions) == 'Hong Kong').reshape(-1)
+            bulk_list = np.array(bulk_list)
+            MongoCollections.read()['Beijing'].bulk_write(list(bulk_list[beijing_index]), ordered=True)
+            MongoCollections.read()['Hong Kong'].bulk_write(list(bulk_list[hk_index]), ordered=True)
         read_file.close()
 
     def init_all(self):
+        print('Initializing user collection...\n')
         self.init_user()
+        print('Initializing article collection...\n')
         self.init_article()
+        print('Initializing read collection...\n')
         self.init_read()
 
 
 if __name__ == '__main__':
-    print(QueryManager.query_user({'uid': '1'}))
     '''init = MongoInit()
-    init.init_article()'''
+    init.init_all()'''
+    print(QueryManager.query_read({}))
+
--- mongodb/query_manager.py
+++ mongodb/query_manager.py
@@ -2,11 +2,22 @@ from config import *
 
 class QueryManager:
     @classmethod
-    def query_user(self, query):
+    def query_user(self, query, field={}):
         cols = list(MongoCollections.user().values())
-        return list(cols[0].find(query)) + list(cols[1].find(query))
+        return list(cols[0].find(query, field)) + list(cols[1].find(query, field))
 
     @classmethod
-    def query_article(self, query):
+    def query_one_user(self, query, field={}):
+        cols = list(MongoCollections.user().values())
+        result = list(cols[0].find(query, field)) + list(cols[1].find(query, field))
+        return result[0]
+
+    @classmethod
+    def query_article(self, query, field={}):
         cols = list(MongoCollections.article().values())
-        return list(cols[0].find(query)) + list(cols[1].find(query))
+        return list(cols[0].find(query, field)) + list(cols[1].find(query, field))
+
+    @classmethod
+    def query_read(self, query, field={}):
+        cols = list(MongoCollections.read().values())
+        return list(cols[0].find(query, field)) + list(cols[1].find(query, field))
