e6aa6b6264672d7780be2763d6e044cf89cb1df3
atfortes <atfortes@protonmail.com>
2021-12-16 21:28:39 +0000

    added files for hadoop article insertion - unfinished

---
+++ hadoop/config.java
@@ -0,0 +1,16 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.stream.Stream;
+
+public class config {
+
+    public static void main(String[] args) throws InterruptedException {
+    }
+}
