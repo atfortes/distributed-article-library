47b1f2e2dc3f4a1487cf399784dfb61764f751ed
davidpissarra <david.pissarra@tecnico.ulisboa.pt>
2021-12-21 02:18:22 +0000

    App almost implemented.

--- app/app_tk.py
+++ app/app_tk.py
@@ -49,10 +49,10 @@ class ArticleApp(tk.Tk):
 
     def fetch_user(self, uid, tklist, found_label):
         tklist.delete(0, tk.END)
-        if uid.get() == '':
+        if uid == '':
             query_id = {}
         else:
-            uids = uid.get().split(',')
+            uids = uid.split(',')
             query_id = {'uid': {'$in': uids}}
         res = QueryManager.query_user(query_id)
 
@@ -153,6 +153,19 @@ class ArticleApp(tk.Tk):
         aids = res[0]['articleAidList']
         self.fetch_article(','.join(aids[:int(top_text.get())]), tklist, found_label, sort=False)
 
+    def fetch_article_user(self, uid, tklist, found_label):
+        tklist.delete(0, tk.END)
+        res = QueryManager.query_read({'uid': uid})
+        aids = set(map(lambda x: x['aid'], res))
+        self.fetch_article(','.join(aids), tklist, found_label)
+
+    def fetch_user_article(self, aid, tklist, found_label):
+        tklist.delete(0, tk.END)
+        res = QueryManager.query_read({'aid': aid})
+        uids = set(map(lambda x: x['uid'], res))
+        self.fetch_user(','.join(uids), tklist, found_label)
+
+
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -205,7 +218,7 @@ class UserPage(tk.Frame):
         user_entry.pack(side=tk.LEFT)
 
         user_submit = tk.Button(top_frame, text='Submit',
-                                command=lambda: controller.fetch_user(user_text, result_list, found_label),
+                                command=lambda: controller.fetch_user(user_text.get(), result_list, found_label),
                                 pady=5, padx=20)
         user_submit.pack(side=tk.LEFT)
 
@@ -519,17 +532,17 @@ class ArticlePage(tk.Frame):
         top_frame = tk.Frame(self)
         top_frame.pack()
 
-        user_label = tk.Label(top_frame, text='Article ID', font=('bold', 14), pady=20, padx=20)
-        user_label.pack(side=tk.LEFT)
+        article_label = tk.Label(top_frame, text='Article ID', font=('bold', 14), pady=20, padx=20)
+        article_label.pack(side=tk.LEFT)
 
         article_text = tk.StringVar()
         article_entry = tk.Entry(top_frame, textvariable=article_text)
         article_entry.pack(side=tk.LEFT)
 
-        user_submit = tk.Button(top_frame, text='Submit',
+        article_submit = tk.Button(top_frame, text='Submit',
                                 command=lambda: controller.fetch_article(article_text.get(), result_list, found_label),
                                 pady=5, padx=20)
-        user_submit.pack(side=tk.LEFT)
+        article_submit.pack(side=tk.LEFT)
 
         found_label = tk.Label(self, text='', font=('bold', 14), padx=20)
         found_label.pack()
@@ -540,9 +553,9 @@ class ArticlePage(tk.Frame):
         buttons = tk.Frame(self)
         buttons.pack(pady=10)
 
-        edit_user_button = tk.Button(buttons, text="Read article",
+        edit_article_button = tk.Button(buttons, text="Read article",
                                      command=lambda: controller.show_frame(ArticlePage))
-        edit_user_button.pack()
+        edit_article_button.pack()
 
         back_button = tk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
@@ -555,9 +568,45 @@ class ReadPage(tk.Frame):
         label = tk.Label(self, text="Reads", font=LARGE_FONT, fg='blue')
         label.pack(pady=10, padx=10)
 
+        top_frame = tk.Frame(self)
+        top_frame.pack()
+
+        user_label = tk.Label(top_frame, text='User ID', font=('bold', 14), pady=20, padx=20)
+        user_label.pack(side=tk.LEFT)
+
+        user_text = tk.StringVar()
+        user_entry = tk.Entry(top_frame, textvariable=user_text)
+        user_entry.pack(side=tk.LEFT)
+
+        user_submit = tk.Button(top_frame, text='Submit User ID',
+                                command=lambda: controller.fetch_article_user(user_text.get(), result_list, found_label),
+                                pady=5, padx=20)
+        user_submit.pack(side=tk.LEFT)
+
+        top_frame = tk.Frame(self)
+        top_frame.pack()
+
+        article_label = tk.Label(top_frame, text='Article ID', font=('bold', 14), pady=20, padx=20)
+        article_label.pack(side=tk.LEFT)
+
+        article_text = tk.StringVar()
+        article_entry = tk.Entry(top_frame, textvariable=article_text)
+        article_entry.pack(side=tk.LEFT)
+
+        article_submit = tk.Button(top_frame, text='Submit Article ID',
+                                command=lambda: controller.fetch_user_article(article_text.get(), result_list, found_label),
+                                pady=5, padx=20)
+        article_submit.pack(side=tk.LEFT)
+
+        found_label = tk.Label(self, text='', font=('bold', 14), padx=20)
+        found_label.pack()
+
+        result_list = tk.Listbox(self, height=8, width=50)
+        result_list.pack()
+
         button1 = tk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+        button1.pack(pady=30)
 
 
 class BeReadPage(tk.Frame):
@@ -569,8 +618,8 @@ class BeReadPage(tk.Frame):
         top_frame = tk.Frame(self)
         top_frame.pack()
 
-        user_label = tk.Label(top_frame, text='Article ID', font=('bold', 14), pady=20, padx=20)
-        user_label.pack(side=tk.LEFT)
+        article_label = tk.Label(top_frame, text='Article ID', font=('bold', 14), pady=20, padx=20)
+        article_label.pack(side=tk.LEFT)
 
         article_text = tk.StringVar()
         article_entry = tk.Entry(top_frame, textvariable=article_text)
